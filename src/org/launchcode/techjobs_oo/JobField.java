16796dd2a5da291e201921a898e2b9f7d2bf6936
Taylor Branson <taylor.a.branson@gmail.com>
2020-09-14 23:27:31 -0500

    implemented abstract class

--- src/org/launchcode/techjobs_oo/JobField.java
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -1,4 +1,59 @@
 package org.launchcode.techjobs_oo;
 
-public class JobField {
+import java.util.Objects;
+
+public abstract class JobField {
+
+    // Fields:
+
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+
+    // Constructors:
+
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    // Custom toString, equals, and hashCode methods:
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JobField jobField = (JobField) o;
+        return id == jobField.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    // Getters and Setters:
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue() {
+        this.value = value;
+    }
 }
